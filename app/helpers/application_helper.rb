63540256893f988a2c3e22cb54c14f7470e54b35
Aftab Hussain Miranda <wafto.mx@gmail.com>
2010-12-11 17:43:36 -0600

    User model with passwords, and user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
     image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
   
+  def gravatar_for(user, options = { :size =>50 })
+    gravatar_image_tag(user.email.downcase, :alt => user.name, :class => 'gravatar', :gravatar => options)
+  end
+  
 end
